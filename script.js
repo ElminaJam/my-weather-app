8c2b3cbb543f10f25762dc293eaf297c86dd05d4
ElminaJam <elminajamalova@gmail.com>
2023-05-04 00:05:22 +0200

    updated js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function showWeather(response) {
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
-    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+    `https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
   iconElement.setAttribute("alt", response.data.condition.description);
 }
